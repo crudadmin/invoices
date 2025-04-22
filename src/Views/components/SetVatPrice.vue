b81633c948b6949a12ad268c1866d49932fd7ab8
Marek Gogol <info@marekgogol.sk>
2025-04-22 11:53:03 +0200

    ref

--- src/Views/components/SetVatPrice.vue
+++ src/Views/components/SetVatPrice.vue
@@ -2,54 +2,59 @@
     <div class="form-group">
         <label>{{ field.name }}</label>
 
-        <input type="integer" step="any" :name="field_key" :value="value" @keyup="onChange" @change="reloadPrices" :placeholder="field.name" class="form-control">
+        <input
+            type="integer"
+            step="any"
+            :name="name"
+            :value="value"
+            @keyup="onChange"
+            @change="reloadPrices"
+            :placeholder="field.name"
+            class="form-control"
+        />
     </div>
 </template>
 
 <script type="text/javascript">
 export default {
-    props : ['field_key', 'field', 'row', 'model'],
+    props: ['field_key', 'field', 'row', 'model', 'name'],
 
-    data(){
-        return {
-
-        }
+    data() {
+        return {};
     },
 
-    mounted(){
-        if ( this.field_key.substr(-3) != 'vat' )
-        {
-            this.$watch('row.vat', function(value){
-                this.reloadPrices(this.row.price||0);
+    mounted() {
+        if (this.field_key.substr(-3) != 'vat') {
+            this.$watch('row.vat', function (value) {
+                this.reloadPrices(this.row.price || 0);
             });
         }
     },
 
-    computed : {
+    computed: {
         //Get input value
-        value(){
+        value() {
             return this.field.value || this.field.default;
         },
     },
 
-    methods : {
+    methods: {
         //Update input value
-        onChange(e){
+        onChange(e) {
             this.field.value = e.target.value;
         },
-        reloadPrices(e){
+        reloadPrices(e) {
             var value = typeof e === 'object' && e.target ? e.target.value : e,
-                vat = parseFloat(this.row.vat||this.model.fields.vat.default);
+                vat = parseFloat(this.row.vat || this.model.fields.vat.default) || 0;
 
-            if ( typeof value == 'string' )
-                value = value.replace(',', '.');
+            if (typeof value == 'string') value = value.replace(',', '.');
 
-            if ( this.field_key.substr(-3) == 'vat' ) {
-                this.row.price = parseFloat((parseFloat(value)||0) / (1 + (vat / 100))).toFixed(2);
+            if (this.field_key.substr(-3) == 'vat') {
+                this.row.price = parseFloat((parseFloat(value) || 0) / (1 + vat / 100)).toFixed(2);
             } else {
-                this.row.price_vat = parseFloat((parseFloat(value)||0) * (1 + (vat / 100))).toFixed(2);
+                this.row.price_vat = parseFloat((parseFloat(value) || 0) * (1 + vat / 100)).toFixed(2);
             }
         },
-    }
-}
+    },
+};
 </script>
