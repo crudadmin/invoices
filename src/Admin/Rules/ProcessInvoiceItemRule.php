0c76d05bbbf5b77e72abf5df4b2f7eb4bf5571e2
Marek Gogol <info@marekgogol.sk>
2025-07-31 15:00:31 +0200

    automatic vat feature added

--- src/Admin/Rules/ProcessInvoiceItemRule.php
+++ src/Admin/Rules/ProcessInvoiceItemRule.php
@@ -4,8 +4,6 @@ namespace Gogol\Invoices\Admin\Rules;
 
 use Admin\Eloquent\AdminModel;
 use Admin\Eloquent\AdminRule;
-use Admin;
-use Ajax;
 
 class ProcessInvoiceItemRule extends AdminRule
 {
@@ -17,7 +15,33 @@ class ProcessInvoiceItemRule extends AdminRule
     //On all events
     public function fire(AdminModel $row)
     {
+        $this->setDefaultVat($row);
         $this->reloadItemPrice($row);
+    }
+
+    private function setDefaultVat($row)
+    {
+        if ( !($row->vat == -1 || !$row->vat) ){
+            return;
+        }
+
+        $invoice = $row->invoice;
+        $subject = $invoice->subject;
+
+        // If country of invoice is different from country of subject and company has VAT, return VAT 0
+        if ( ($invoice->country_id != $subject->country_id) && $invoice->company_vat_id ) {
+            $row->vat = 0;
+        }
+
+        // Default VAT for chosen subject
+        else if ( $subject->vat_default_id ) {
+            $row->vat = $subject->vat_default->vat;
+        }
+
+        // Set zero vat if no setting has been found
+        else {
+            $row->vat = 0;
+        }
     }
 
     private function reloadItemPrice($row)
